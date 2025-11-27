f8f99d6e1851663af4872e765fc74abe3ca30bcc
kraxd <alvarezpahm28@gmail.com>
2025-11-27 19:53:15 +0700

    Refactor app.py to improve data loading and UI

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ Hãy trả lời tự nhiên, thân thiện, chính xác , chỉ sử dụng ti�
     # 🤖 GỌI OPENROUTER + DEEPSEEK
     # ======================================
 
-    OPENROUTER_API_KEY = "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"
+    OPENROUTER_API_KEY = "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"
 
     url = "https://openrouter.ai/api/v1/chat/completions"
     headers = {
@@ -199,6 +199,7 @@ Hãy trả lời tự nhiên, thân thiện, chính xác , chỉ sử dụng ti�
     else:
         st.error("⚠️ Không thể tải dữ liệu thời tiết!")
         
+
 
 
 
