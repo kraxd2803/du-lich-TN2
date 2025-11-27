2f96a05e3a8ba701ab474bcecfd6c8b8d567fa7f
kraxd <alvarezpahm28@gmail.com>
2025-11-27 20:16:24 +0700

    Refactor app.py for improved readability and structure

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ Hãy trả lời tự nhiên, thân thiện, chính xác , chỉ sử dụng ti�
     # 🤖 GỌI OPENROUTER + DEEPSEEK
     # ======================================
 
-    OPENROUTER_API_KEY = "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"
+    OPENROUTER_API_KEY = st.secrets["OPENROUTER_API_KEY"]
 
     url = "https://openrouter.ai/api/v1/chat/completions"
     headers = {
@@ -199,6 +199,7 @@ Hãy trả lời tự nhiên, thân thiện, chính xác , chỉ sử dụng ti�
     else:
         st.error("⚠️ Không thể tải dữ liệu thời tiết!")
         
+
 
 
 
