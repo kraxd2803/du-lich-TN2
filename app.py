4fd9013f8633c25758527323652309f90818e181
kraxd <alvarezpahm28@gmail.com>
2025-12-02 18:46:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from datetime import datetime
 # ======================================
 # CONFIG GEMINI
 # ======================================
-MODEL_NAME = "gemini-2.5-pro"
+MODEL_NAME = "gemini-2.5-flash"
 client = genai.Client(
     api_key=st.secrets["gemini_key"],
 )
@@ -255,4 +255,5 @@ Hãy trả lời ngắn gọn, mạch lạc và thân thiện.
             st.info(f"🌧️ Khả năng mưa: **{prob}%**")
     else:
         st.warning("Không lấy được dữ liệu thời tiết.")
+
 
